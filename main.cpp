f25d9e6af453da698ad965e24d94dbb43b5bfbda
arColm <jacob.leung@mail.mcgill.ca>
2024-08-23 19:31:45 -0400

    added dear imgui

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@
 #include <src/World/IslandChunkGenerator.h>
 #include<thread>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 using namespace VoxelEngine;
 
 /*===============================
@@ -112,6 +116,7 @@ int main() {
 
 		update(deltaTime);
 
+
 		glfwPollEvents();
 	}
 
@@ -127,9 +132,29 @@ int main() {
 
 void renderingLoop(GLFWwindow* window, World* world) {
 	glfwMakeContextCurrent(window);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplOpenGL3_Init();
+
+
 	//mainCamera->forceUpdateCurrentChunk();
 	while (!glfwWindowShouldClose(window)) {
-
+		// (Your code calls glfwPollEvents())
+		// ...
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::ShowDemoWindow(); // Show demo window! :)
 
 		/*===============================
 			RENDERING
@@ -138,8 +163,21 @@ void renderingLoop(GLFWwindow* window, World* world) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		//mainCamera->updateCurrentChunk();
 		world->renderChunks();
+
+
+		// Rendering
+		// (Your code clears your framebuffer, renders your other stuff etc.)
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		// (Your code calls glfwSwapBuffers() etc.)
+
+
 		glfwSwapBuffers(window);
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 }
 void chunkLoadingLoop(GLFWwindow* window, World* world) {
