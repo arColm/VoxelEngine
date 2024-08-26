d69787302feb9b7b96e6b53090c385fa7f74dff8
arColm <jacob.leung@mail.mcgill.ca>
2024-08-26 13:36:40 -0400

    added fog

--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -1,7 +1,7 @@
 
 #version 430 core
 
-const float fogDistance = 100.0;
+const float fogDistanceSquared = 60000.0;
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec4 aColor;
 //layout (location = 1) in vec2 aTexCoord;
@@ -16,14 +16,14 @@ uniform mat4 projection;
 uniform vec3 cameraPos;
 
 vec3 fogColor(vec3 position) {
-	return  vec3(distance(aPos,cameraPos)/fogDistance);
+	vec2 distanceVector = aPos.xz - cameraPos.xz;
+	return  vec3(dot(distanceVector,distanceVector)/fogDistanceSquared);
 }
 
 void main()
 {
-	vec3 position = (projection * view * vec4(aPos,1.0)).xyz;
 	gl_Position = projection * view * vec4(aPos,1.0);
-	vertexColor = aColor + vec4(fogColor(position),0.0);
+	vertexColor = aColor + vec4(fogColor(aPos),0.0);
 
 	//vertexColor = vec4(aPos, 1.0);
 	//TexCoord = aTexCoord;
